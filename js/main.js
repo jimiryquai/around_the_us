f5df1e00fd832ab878ac2fdb90fbd0729b6bc665
jimiryquai <hello@jamesryanfitness.com>
2020-04-25 16:16:16 +0100

    Added new place form feature

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 const editButton = document.querySelector('.button_edit');
 const closeButton = document.querySelector('.popup__close');
-// get and set form values
-const cardsContainer = document.querySelector(".photo-grid");
-const formElement = document.querySelector('.form');
-const nameInput = formElement.querySelector('.form__input_name');
-const jobInput = formElement.querySelector('.form__input_job');
+const popup = document.querySelector('.popup');
+const editFormDialog = popup.querySelector('.popup__dialog_edit')
+const editForm = document.querySelector('.form_edit');
+const addFormDialog = popup.querySelector('.popup__dialog_add');
+const addForm = document.querySelector('.form_add');
+const nameInput = editForm.querySelector('.form__input_name');
+const jobInput = editForm.querySelector('.form__input_job');
 const name = document.querySelector('.profile__name');
 const job = document.querySelector('.profile__job');
-// Initial card values
+const titleInput = addForm.querySelector('.form__input_title');
+const urlInput = addForm.querySelector('.form__input_url');
+const cardsContainer = document.querySelector('.photo-grid');
 const initialCards = [
     {
         name: "Yosemite Valley",
@@ -34,23 +38,14 @@ const initialCards = [
         link: "https://code.s3.yandex.net/web-code/lago.jpg"
     }
 ];
-//Build card
-
-function buildCard () {
-    const cardTemplate = document.querySelector('#card-template').content;
-    const cardElement = cardTemplate.cloneNode(true);
-    cardElement.querySelector(".photo-grid__image").src = initialCards.link;
-    cardElement.querySelector(".photo-grid__title").textContent = initialCards.name;
-    cardsContainer.append(cardElement);
-}
 
 // Add six initial cards on load
 window.onload = (event) => {
     initialCards.forEach(function (item) {
         const cardTemplate = document.querySelector('#card-template').content;
         const cardElement = cardTemplate.cloneNode(true);
-        const cardImage = cardElement.querySelector(".photo-grid__image");
-        const cardTitle =  cardElement.querySelector(".photo-grid__title");
+        const cardImage = cardElement.querySelector('.photo-grid__image');
+        const cardTitle =  cardElement.querySelector('.photo-grid__title');
         cardImage.src = item.link;
         cardImage.alt = item.name;
         cardTitle.textContent = item.name;
@@ -60,37 +55,89 @@ window.onload = (event) => {
 
 // Toggles class of popup_opened
 function popupToggle() {
-    let popup = document.querySelector('.popup')
     popup.classList.toggle('popup_opened');
 }
 
-editButton.addEventListener("click", popupToggle);
-closeButton.addEventListener("click", popupToggle);
+//Toggle visibility of edit form
+function editFormVisibility () {
+    editFormDialog.classList.toggle('popup__dialog_visible');
+}
 
+//Toggle visibility of add form
+function addFormVisibility () {
+    addFormDialog.classList.toggle('popup__dialog_visible');
+}
 
-function formLoadHandler() {
+//Populates input elements on edit form
+function editFormValues() {
     let nameText = name.textContent;
     let jobText = job.textContent;
-
     nameInput.value = nameText;
     jobInput.value = jobText;
 }
 
-editButton.addEventListener('click', formLoadHandler);
-
 // Next is the form submit handler, though
 // it won't submit anywhere just yet
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // This line stops the browser from submitting the form in the default way.
-                                                // Having done so, we can define our own way of submitting the form.
-                                                // We'll explain it in more detail later
+function addFormSubmit (evt) {
+    evt.preventDefault(); 
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.cloneNode(true);
+    const cardImage = cardElement.querySelector('.photo-grid__image');
+    const cardTitle =  cardElement.querySelector('.photo-grid__title');
+    cardImage.src = urlInput.value;
+    cardImage.alt = titleInput.value;
+    cardTitle.textContent = titleInput.value;
+    cardsContainer.append(cardElement);
+}
 
-    // Insert new values using the textContent property of the querySelector() method
+function addFormClear () {
+    urlInput.value = "";
+    titleInput.value = "";
+}
+
+function editFormSubmit (evt) {
+    evt.preventDefault(); 
     name.textContent = nameInput.value;
     job.textContent = jobInput.value;
 }
 
-// Connect the handler to the form:
-// it will watch the submit event
-formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', popupToggle);
+//Perform multiple actions on edit button click
+//https://gomakethings.com/attaching-multiple-elements-to-a-single-event-listener-in-vanilla-js/
+document.addEventListener('click', function (evt) {
+    if ( evt.target.classList.contains( 'button_edit' ) ) {
+        popupToggle();
+        editFormVisibility();
+        editFormValues();
+    }
+}, false);
+
+document.addEventListener('click', function (evt) {
+    if ( evt.target.classList.contains( 'button_add' ) ) {
+        popupToggle();
+        addFormVisibility();
+    }
+}, false);
+
+//Perform multiple actions on popup close button click
+document.addEventListener('click', function (evt) {
+    if ( evt.target.classList.contains( 'popup__close_edit' ) ) {
+        editFormVisibility();
+        popupToggle();
+    }
+}, false);
+
+document.addEventListener('click', function (evt) {
+    if ( evt.target.classList.contains( 'popup__close_add' ) ) {
+        addFormVisibility();
+        popupToggle();
+    }
+}, false);
+
+//Edit form submission listeners
+editForm.addEventListener('submit', editFormSubmit);
+editForm.addEventListener('submit', popupToggle);
+editForm.addEventListener('submit', editFormVisibility);
+addForm.addEventListener('submit', addFormSubmit);
+addForm.addEventListener('submit', popupToggle);
+addForm.addEventListener('submit', addFormVisibility);
+addForm.addEventListener('submit', addFormClear);
